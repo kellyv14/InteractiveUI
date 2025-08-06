77eaaab68965e9364553553d4384df0d51017d1f
Kelly <kelvu14@gmail.com>
2025-08-06 15:12:36 -0400

    added age option

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -10,29 +10,39 @@ import SwiftUI
 struct ContentView: View {
     @State private var name = ""
     @State private var textTitle = "What is your name?"
+    @State private var age = ""
+    @State private var textAge = "How old are you?"
     var body: some View {
         VStack {
-            
+
     Text(textTitle)
         .font(.title)
-            
+        
     TextField("Type your name here...", text: $name)
         .multilineTextAlignment(.center)
         .font(.title)
         .border(Color.gray, width:1)
+         
+    Text(textAge)
+        .font(.title)
             
-    Button("Submit Name") {
-        textTitle = "Welcome, \(name)!"
-        name = ""
+    TextField("Type age here...", text: $age)
+        .multilineTextAlignment(.center)
+        .font(.title)
+        .border(Color.gray, width:1)
+            
+    Button("Submit Name and Age") {
+        textTitle = "Welcome, \(name)! You are \(age) years old!"
+        textAge = ""
+    name = ""
+    age = ""
+        
             }
         .font(.title2)
         .buttonStyle(.borderedProminent)
         .tint(.purple)
 
-            
-            
-            
-            
+    
         }
         .padding()
     }
